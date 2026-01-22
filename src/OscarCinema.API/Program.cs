8390ca28cefca46044762a8412a45799013a68e5
Guilherme Reis Videira <guilhermereisdl@gmail.com>
2026-01-22 15:08:28 -0300

    Fixing database connection URL for deploy

--- src/OscarCinema.API/Program.cs
+++ src/OscarCinema.API/Program.cs
@@ -37,12 +37,32 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-var connectionString =
-    builder.Configuration["DATABASE_URL"]
-    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+var databaseUrl = builder.Configuration["DATABASE_URL"];
+
+string connectionString;
+
+if (!string.IsNullOrWhiteSpace(databaseUrl))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+
+    connectionString =
+        $"Host={uri.Host};" +
+        $"Port={uri.Port};" +
+        $"Database={uri.AbsolutePath.TrimStart('/')};" +
+        $"Username={userInfo[0]};" +
+        $"Password={userInfo[1]};" +
+        $"SSL Mode=Require;" +
+        $"Trust Server Certificate=true";
+}
+else
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
 
 builder.Services.AddDbContext<OscarCinemaContext>(options =>
     options.UseNpgsql(connectionString));
+
 
 builder.Services.AddAutoMapper(cfg =>
 {
